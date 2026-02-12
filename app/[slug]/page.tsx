0548ed13ad7c9b8c90e23d28e648bce9850d1ece
thebalayogesh <thebalayogesh@gmail.com>
2026-02-13 00:49:58 +0530

    new commmit

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import '../styles.css';
+import React from 'react';
+import Markdoc from '@markdoc/markdoc';
+import { reader } from '../reader';
+import { markdocConfig } from '../../keystatic.config';
+
+export default async function Post(props: {
+  params: Promise<{ slug: string }>;
+}) {
+  const params = await props.params;
+  const { slug } = params;
+
+  const post = await reader.collections.posts.read(slug);
+
+  if (!post) return <div>Post not found!</div>;
+
+  const { node } = await post.content();
+
+  const errors = Markdoc.validate(node, markdocConfig);
+  if (errors.length) {
+    console.error(errors);
+    throw new Error('Invalid content');
+  }
+
+  const renderable = Markdoc.transform(node, markdocConfig);
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      {Markdoc.renderers.react(renderable, React)}
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  const slugs = await reader.collections.posts.list();
+
+  return slugs.map(slug => ({
+    slug,
+  }));
+}
---
+++ keystatic.config.ts
@@ -0,0 +1,21 @@
+import { config, collection, fields } from '@keystatic/core';
+
+export const markdocConfig = fields.markdoc.createMarkdocConfig({});
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({ label: 'Content' }),
+      },
+    }),
+  },
+});
